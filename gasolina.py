a2f58dae681a30b7b7cc0c18f4e5371ee80e7f46
FellipeOlliveira <marcosf1625@gmail.coom>
2026-02-17 15:42:23 +0000

    UPDATE: Gasolina.py and Gasolina.png

--- gasolina.py
+++ gasolina.py
@@ -7,10 +7,17 @@ df = pd.read_csv("gasolina.csv", delimiter=',')
 sns.set(style="whitegrid")
 
 plt.figure(figsize=(8,5))
-sns.lineplot(data=df, x="dia", y="venda")
 
-plt.title("Vendas por Dia")
-plt.xlabel("Dia")
-plt.ylabel("Venda")
+sns.lineplot(
+    data=df,
+    x="dia",
+    y="venda",
+    label="Preço da Gasolina (R$)"
+)
 
+plt.title("Variação do Preço da Gasolina por Dia")
+plt.xlabel("Dia do Mês")
+plt.ylabel("Preço (R$)")
+
+plt.legend(title="Legenda")
 plt.show()
